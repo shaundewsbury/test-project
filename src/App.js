3836d4fa6a1e12e8618d10e1b170b67f9db10dca
shaundewsbury <shaundewsbury@hotmail.co.uk>
2022-09-23 15:05:04 +0100

    deploy test project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Heading from './components/Heading';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="mainContent">
+        <Heading title="Test Project"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Heading.js
@@ -0,0 +1,7 @@
+const Heading = (props) => {
+    return (
+        <h1>{props.title}</h1>
+    )
+}
+
+export default Heading;
